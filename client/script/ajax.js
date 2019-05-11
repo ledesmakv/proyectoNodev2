3b46e6c091ef3ad5733f2eccb26229dfbb42d3f5
Karina <ledesmakv@gmail.com>
2019-05-10 22:17:12 -0300

    second commit

--- client/script/ajax.js
+++ client/script/ajax.js
@@ -1,12 +1,26 @@
 function pedirTodosLosKillers(callback) {
     let request = new XMLHttpRequest();
 
-    request.onload = function () {
+    request.onload = () => {
         let todosLosKillers = JSON.parse(request.responseText);
 
         callback(todosLosKillers);
     }
 
     request.open('GET', "/killers");
+    request.send();
+}
+
+
+function pedirTodosLosSurvivors(callback) {
+    let request = new XMLHttpRequest();
+
+    request.onload = () => {
+        let todosLosSurvivors = JSON.parse(request.responseText);
+
+        callback(todosLosSurvivors);
+    }
+
+    request.open('GET', "/survivors");
     request.send();
 }
--- client/script/script.js
+++ client/script/script.js
@@ -3,8 +3,54 @@
 //////////////////////////////////////////////
 
 
+// Guardamos nuestros elementos en variables
+
 let miBotonSurvivors = document.getElementById('survivorsBtn');
 const contenedorSurvivors = document.getElementById('survivorsContainer');
+
+
+// Añadimos un evento el cual nos trae los datos y creamos el botón que contiene al grupo
+
+miBotonSurvivors.addEventListener('click', () => {
+
+    pedirTodosLosSurvivors(crearBotonesSurvivors);
+
+})
+
+function crearBotonesSurvivors (datos) {
+
+    let arrayDeSurvivors = datos;
+
+    for (let i = 0; i < arrayDeSurvivors.length; i++) {
+
+        crearBotonSurvivors(arrayDeSurvivors[i].name, arrayDeSurvivors[i].image);
+
+    }
+}
+
+
+// Creamos un div para cada survivor junto con el nombre y la imagen
+
+function crearBotonSurvivors(nombreSurvivor, imagenSurvivor) {
+
+    let divSurvivor = document.createElement('div');
+
+    divSurvivor.setAttribute('class', "boton-survivor");
+
+    let texto = document.createTextNode(nombreSurvivor);
+
+    divSurvivor.appendChild(texto);
+    contenedorSurvivors.appendChild(divSurvivor);
+
+    let dirImgSurvivor = document.createElement('img');
+
+    dirImgSurvivor.setAttribute('class', "imgSurvivor");
+    dirImgSurvivor.setAttribute('src', imagenSurvivor);
+    dirImgSurvivor.setAttribute('alt', nombreSurvivor);
+
+    contenedorSurvivors.appendChild(dirImgSurvivor);
+
+}
 
 
 //////////////////////////////////////////////
@@ -12,23 +58,38 @@ const contenedorSurvivors = document.getElementById('survivorsContainer');
 //////////////////////////////////////////////
 
 
+// Guardamos nuestros elementos en variables
+
 let miBoton = document.getElementById('killersBtn');
 const contenedorKillers = document.getElementById('killersContainer');
 
-miBoton.addEventListener('click', function () {
-    pedirTodosLosKillers(crearBotones);
+
+// Añadimos un evento el cual nos trae los datos y creamos el botón que contiene al grupo
+
+miBoton.addEventListener('click', () => {
+
+    pedirTodosLosKillers(crearBotonesKillers);
+
 })
 
-function crearBotones (datos) {
+function crearBotonesKillers (datos) {
+
     let arrayDeKillers = datos;
 
     for (let i = 0; i < arrayDeKillers.length; i++) {
-        crearBoton(arrayDeKillers[i].name, arrayDeKillers[i].image);
+
+        crearBotonKiller(arrayDeKillers[i].name, arrayDeKillers[i].image);
+
     }
 }
 
-function crearBoton(nombreKiller, imagenKiller) {
+
+// Creamos un div para cada survivor junto con el nombre y la imagen
+
+function crearBotonKiller(nombreKiller, imagenKiller) {
+
     let divKiller = document.createElement('div');
+
     divKiller.setAttribute('class', "boton-killer");
 
     let texto = document.createTextNode(nombreKiller);
@@ -37,9 +98,11 @@ function crearBoton(nombreKiller, imagenKiller) {
     contenedorKillers.appendChild(divKiller);
 
     let dirImgKiller = document.createElement('img');
+    
     dirImgKiller.setAttribute('class', "imgKiller");
     dirImgKiller.setAttribute('src', imagenKiller);
     dirImgKiller.setAttribute('alt', nombreKiller);
 
     contenedorKillers.appendChild(dirImgKiller);
+
 }
